cf9f0c85a2313b77f6c2f6c660e393e52dc54c9d
SRIJAN SAHU <90348563+theSrijan13@users.noreply.github.com>
2022-11-05 00:50:01 +0530

    Add files via upload

---
+++ Quicksort1.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+
+void swap(int arr[],int i,int j)
+{
+    int temp=arr[i];
+    arr[i]=arr[j];
+    arr[j]=temp;
+}
+int partition(int arr[],int l,int r)
+{
+    int pivot=arr[r];
+    int i = l-1;
+    for(int j=1;j<r;j++)
+    {
+        if(arr[j]<pivot)
+        {
+            i++;
+            swap(arr,i,j);
+        }
+    }
+    swap(arr,i+1,r);
+    return i+1;
+}
+void Quicksort(int arr[],int l,int r)
+{
+    if(l<r)
+    {
+        int pi = partition(arr,l,r);
+        Quicksort(arr,l,pi-1);
+        Quicksort(arr,pi+1,r);
+    }
+}
+int main()
+{
+    int arr[5]={5,4,3,2,1};
+    Quicksort(arr,0,4);
+    for(int i=0;i<5;i++)
+    {
+        cout<<arr[i]<<" ";
+
+    }
+    cout<<endl;
+    return 0;
+}
