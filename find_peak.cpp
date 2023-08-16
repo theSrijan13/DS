d9b73e1823405efbcf0b12e0a164d364bad5aeb8
SRIJAN SAHU <90348563+theSrijan13@users.noreply.github.com>
2023-08-17 01:14:35 +0530

    Add files via upload

---
+++ find_peak.cpp
@@ -0,0 +1,30 @@
+class Solution{
+    public:
+    int findpeak(vector<int> nums){
+        int n = nums.size();
+        if(n==1){
+            return 0;            
+        }
+        if(nums[0] > nums[1]){
+            return 0;
+        }
+        if(nums[n-1] > nums[n-2]){
+            return n-1;
+        }
+        int s = 1;
+        int e = n-1;
+        while(s<=e){
+            int mid = s + (e-s)/2;
+            if(nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1]){
+                return mid;
+            }
+            else if(nums[mid] < nums[mid-1]){
+                s = mid+1;
+            }
+            else{
+                e = mid -1;
+            }
+        }
+        return -1;
+    }
+}
