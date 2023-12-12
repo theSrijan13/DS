078a8431089836078584ef2b747721e26c218f67
SRIJAN SAHU <90348563+theSrijan13@users.noreply.github.com>
2023-12-12 23:12:20 +0530

    Time: 8 ms (10.42%), Space: 7.6 MB (76.86%) - LeetHub

---
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        stack<int> st;
+        int ans = 0;
+        st.push(-1);
+        for(int i =0;i<s.length();i++){
+            if(s[i] == '('){
+                st.push(i);
+            }
+            else{
+                if(st.size()==1){
+                    st.top() = i;
+                }
+                else{
+                    st.pop();
+                    ans = max(ans,i - st.top());
+                }
+            }
+        }
+        return ans;
+    }
+};
