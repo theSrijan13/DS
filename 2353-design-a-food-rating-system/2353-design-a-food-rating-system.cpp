03f5b0374b31c2711829bfd9a1645479d9673a83
SRIJAN SAHU <90348563+theSrijan13@users.noreply.github.com>
2023-12-17 10:32:50 +0530

    Time: 368 ms (90.43%), Space: 162.7 MB (78.47%) - LeetHub

---
+++ 2353-design-a-food-rating-system/2353-design-a-food-rating-system.cpp
@@ -0,0 +1,66 @@
+class Food {
+public:
+    // Store the food's rating.
+    int foodRating;
+    // Store the food's name.
+    string foodName;
+
+    Food(int foodRating, string foodName) {
+        this->foodRating = foodRating;
+        this->foodName = foodName;
+    }
+
+    // Overload the less than operator for comparison
+    bool operator<(const Food& other) const {
+        // If food ratings are same sort on the basis of their name. (lexographically smaller name food will be on top)
+        if (foodRating == other.foodRating) {
+            return foodName > other.foodName;
+        }
+        return foodRating < other.foodRating;
+    }
+};
+class FoodRatings {
+public:
+    unordered_map<string, int> foodRatingMap;
+    // Map food with cuisine it belongs to.
+    unordered_map<string, string> foodCuisineMap;
+    
+    // Store all food of a cusine in priority queue (to sort them on ratings/name)
+    // Priority queue element -> Food: (foodRating, foodName)
+    unordered_map<string, priority_queue<Food>> cuisineFoodMap;
+    FoodRatings(vector<string>& foods, vector<string>& cuisines, vector<int>& ratings) {
+        for (int i = 0; i < foods.size(); ++i) {
+            // Store 'rating' and 'cuisine' of current 'food' in 'foodRatingMap' and 'foodCuisineMap' maps.
+            foodRatingMap[foods[i]] = ratings[i];
+            foodCuisineMap[foods[i]] = cuisines[i];
+            // Insert the '(rating, name)' element in current cuisine's priority queue.
+            cuisineFoodMap[cuisines[i]].push(Food(ratings[i], foods[i]));
+        }
+        
+    }
+    
+    void changeRating(string food, int newRating) {
+        foodRatingMap[food] = newRating;
+        // Insert the '(new rating, name)' element in respective cuisine's priority queue.
+        auto cuisineName = foodCuisineMap[food];
+        cuisineFoodMap[cuisineName].push(Food(newRating, food));
+        
+    }
+    
+    string highestRated(string cuisine) {
+        
+        auto highestRated = cuisineFoodMap[cuisine].top();
+        while (foodRatingMap[highestRated.foodName] != highestRated.foodRating) {
+            cuisineFoodMap[cuisine].pop();
+            highestRated = cuisineFoodMap[cuisine].top();
+        }
+        return highestRated.foodName;
+    }
+};
+
+/**
+ * Your FoodRatings object will be instantiated and called as such:
+ * FoodRatings* obj = new FoodRatings(foods, cuisines, ratings);
+ * obj->changeRating(food,newRating);
+ * string param_2 = obj->highestRated(cuisine);
+ */
